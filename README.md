e3d5fd2912066f2f6b14f75e5bbf5f0b3a1c1c70
william <xiningbank@gmail.com>
2023-04-05 22:29:54 +0800

    add README.md for the client configurations.

---
+++ README.md
@@ -0,0 +1,9 @@
+For client configurations, see: https://github.com/hwdsl2/setup-ipsec-vpn/blob/master/docs/clients.md
+and here's the troubleshooting section: https://github.com/hwdsl2/setup-ipsec-vpn/blob/master/docs/clients.md#troubleshooting
+
+An instance of the configuration for ubuntu:
+Phase1 Algorithms: aes128-sha1-modp2048
+Phase2 Algorithms: aes128-sha1
+or
+Phase1 Algorithms: aes-sha1-modp1024
+Phase2 Algorithms: aes-sha1
--- install.sh
+++ install.sh
@@ -14,12 +14,3 @@ docker pull fcojean/l2tp-ipsec-vpn-server
 
 # create docker container
 docker run --name l2tp-ipsec-vpn-server --env-file ./.env -p 500:500/udp -p 4500:4500/udp -v /lib/modules:/lib/modules:ro -d --privileged fcojean/l2tp-ipsec-vpn-server
-
-# for client configurations, see: https://github.com/hwdsl2/setup-ipsec-vpn/blob/master/docs/clients.md
-# for client configuration/connection issue, see trouble shootting: https://github.com/hwdsl2/setup-ipsec-vpn/blob/master/docs/clients.md#troubleshooting
-# for example(ubuntu):
-# Phase1 Algorithms: aes128-sha1-modp2048
-# Phase2 Algorithms: aes128-sha1
-# or
-# Phase1 Algorithms: aes-sha1-modp1024
-# Phase2 Algorithms: aes-sha1
